d24a53dae3b86032d0d8e6de4544e0eb522634ab
themba <seconds.seconds1@gmail.com>
2023-03-17 10:57:12 -0700

    7fix

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -4,5 +4,5 @@ word_first_3 = "Hol"
 word_last_2 = "on"
 middle_word = "olbeto"
 print(f"First 3 letters: {word_first_3}")
-print(f"Last 2 letters: {word_last}")
+print(f"Last 2 letters: {word_last_2}")
 print(f"Middle word: {middle_word}")
