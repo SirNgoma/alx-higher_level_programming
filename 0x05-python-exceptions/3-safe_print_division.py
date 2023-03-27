d935801d4b93312f6d7b802c9146551ebad1280b
Themba <seconds.seconds1@gmail.com>
2023-03-27 12:26:38 -0700

    niceeee

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -3,6 +3,7 @@ def safe_print_division(a, b):
     try:
         r = a / b
     except ZeroDivisionError:
-        print("divion by zero")
+        r = None
     finally:
         print("Inside result: {}".format(r))
+        return r
--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,30 +1,29 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
-    st_division(my_list_1, my_list_2, list_length):
-            new_list = []
-                for i in range(list_length):
-                            try:
-                                            elem_1 = my_list_1[i]
-                                                    except IndexError:
-                                                                    print("out of range")
-                                                                                elem_1 = 0
-                                                                                        try:
-                                                                                                        elem_2 = my_list_2[i]
-                                                                                                                except IndexError:
-                                                                                                                                print("out of range")
-                                                                                                                                            elem_2 = 0
-                                                                                                                                                    try:
-                                                                                                                                                                    result = elem_1 / elem_2
-                                                                                                                                                                                if isinstance(result, (int, float)):
-                                                                                                                                                                                                    new_list.append(result)
-                                                                                                                                                                                                                else:
-                                                                                                                                                                                                                                    print("wrong type")
-                                                                                                                                                                                                                                                    new_list.append(0)
-                                                                                                                                                                                                                                                            except ZeroDivisionError:
-                                                                                                                                                                                                                                                                            print("division by 0")
-                                                                                                                                                                                                                                                                                        new_list.append(0)
-                                                                                                                                                                                                                                                                                                except TypeError:
-                                                                                                                                                                                                                                                                                                                print("wrong type")
-                                                                                                                                                                                                                                                                                                                            new_list.append(0)
-                                                                                                                                                                                                                                                                                                                                return new_list
+    new_list = []
 
+    for i in range(list_length):
+        try:
+            elem_1 = my_list_1[i]
+        except IndexError:
+            print("out of range")
+            elem_1 = 0
+        try:
+            elem_2 = my_list_2[i]
+        except IndexError:
+            print("out of range")
+            elem_2 = 0
+        try:
+            result = elem_1 / elem_2
+            if isinstance(result, (int, float)):
+                new_list.append(result)
+            else:
+                print("wrong type")
+                new_list.append(0)
+        except ZeroDivisionError:
+            print("division by 0")
+            new_list.append(0)
+        except TypeError:
+            print("wrong type")
+            new_list.append(0)
+    return new_list
