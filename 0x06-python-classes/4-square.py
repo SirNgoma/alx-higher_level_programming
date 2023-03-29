b3be430239d08ee9cffc35f96eb635fca274323e
Themba <seconds.seconds1@gmail.com>
2023-03-29 11:12:12 -0700

    t

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,2 +1,33 @@
 #!/usr/bin/python3
 
+"""Class of a squre."""
+class Square:
+    """Class for a squre."""
+    def __init__(self, size):
+        """Init the size of the Square.
+        
+        Args:
+            size (int): size
+            """
+        self.size = size
+
+    def size(self):
+        """Square size"""
+        return self.size
+
+    def size(self, value):
+         """size of the Square.
+
+         Args:
+             value (int): size of the square.
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.size = value
+        
+    def area(self):
+        """Return area of the square."""
+        return (self.size ** 2)
