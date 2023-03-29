e106566883bed59e32c7fc4a7c69b561810aec74
Themba <seconds.seconds1@gmail.com>
2023-03-28 19:54:50 -0700

    a

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-"""Squre class that is a squre"""
+"""Squre class that is a squre."""
+
 
 class Square:
-    """ this is a Empty class"""
+    """this is a Empty class."""
     pass
