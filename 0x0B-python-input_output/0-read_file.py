61731240219a767d496bb44537a434d4a9786af8
Themba <seconds.seconds1@gmail.com>
2023-04-12 08:55:41 -0700

    gg

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" """
+def read_file(filename=""):
+    """ raed texts from a file
+    Args:
+        filename: name of the file to be read
+        """
+
---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" """
+def from_json_string(my_str):
+    """reurns object rep by json string
+    Args:
+        my_str(string): json string
+    """
+
