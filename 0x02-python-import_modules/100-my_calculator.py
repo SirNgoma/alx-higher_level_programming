8ede705e447b7201f635ec571cba5102cdf156cf
Themba <seconds.seconds1@gmail.com>
2023-04-19 11:40:10 -0700

    hh

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,7 +2,7 @@
 import sys
 if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
-    
+
     if len(sys.argv) != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,15 +2,14 @@
 import sys
 
 if __name_ == "__main__":
-    args = sys.argv[1:]
-    num_of_args = len(args)
+    num_of_args = len(sys.argv) - 1
 
-    print("{} arguments:".format("" if num_of_args == 1 else "s", num_of_args), end="")
+    print(f"{} arguments{'s' if num_of_args != 1 else ''}: {num_of_args}", end="")
     if num_of_args == 0:
         print("0 arguments")
     elif num_of_args == 1:
         print("1 argument")
     else:
         print(":")
-        for i, arg in enumurate(args):
-            print("{}: {}".format(i+1, arg))
+        for i, in range(1, num_of_args + 1):
+            print(f"{i}: {sys.argv[i]}")
