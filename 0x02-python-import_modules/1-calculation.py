f632645914114e98167a8176f6288c9c7ce16378
Themba <seconds.seconds1@gmail.com>
2023-04-19 09:48:57 -0700

    hl

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -14,7 +14,7 @@ print("{} - {} = {}".format(a, b, substract))
 print("{} * {} = {}".format(a, b, multi))
 print("{} / {} = {}".format(a, b, divide))
 
-if __main__ == '__main__':
+if __main__ == "__main__":
     import sys
     add(a, b)
     sub(a, b)
