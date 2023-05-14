18ba34d7ffa488a7c9d9add36ca778599d3c63cd
Themba <seconds.seconds1@gmail.com>
2023-05-14 01:14:00 -0700

    ff

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -2,7 +2,7 @@
 
 """
 function Add_integer() adds two intergers.For Example:
-    >>>add(1,1)
+    >>> add_integer(1,1)
     2
 """
 
@@ -10,17 +10,19 @@ def add_integer(a, b=98):
     """
     returns the addition of two numbers.
 
-    TypeError: must be an integer or b must be an integer;
+    >>> add_integer(2)
+    >>> add_integer(1,2)
+    3
+    >>> add_integer("t",1)
+    Traceback (most recent call last):
+        ...
+    TypeError: a must be an integer or b must be an integer
     """
     if not isinstance(a, (int, float)):
-        raise TypeError("a must be an integer or b must be an integer")
+        raise TypeError("a must be an integer")
     if not isinstance(b,(int, float)):
-        raise TypeError("a must be an integer or b must be an integer")
+        raise TypeError("b must be an integer")
 
     a = int(a)
     b = int(b)
     return a + b
-
-if __name__ = '__main__':
-    import doctest
-    doctest.testmod()
