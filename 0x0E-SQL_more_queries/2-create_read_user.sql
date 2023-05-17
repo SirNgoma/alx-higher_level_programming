d037a823110a09e23a482fbf12f8bf5b5ff0ff69
Themba <seconds.seconds1@gmail.com>
2023-05-17 12:14:28 -0700

    draft alk

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,3 +1,4 @@
 -- creates a DB hbtn_0d_2 and user_0d_2
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-GRANT SELECT TO user_0d_2@localhost IF NOT EXISTS;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
