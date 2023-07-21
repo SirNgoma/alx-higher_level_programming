6c11b6d0fa55381538362fb4a18e36addd5acc8b
Themba <seconds.seconds1@gmail.com>
2023-07-21 16:13:07 +0000

    not tested

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,2 +1,6 @@
 #!/usr/bin/node
+let myVar = `C is fun
+Python is cool
+JavaScript is amazing`;
+console.log(myVar);
 
--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+function tee(my)
+{
+if (arguments ===)
+{
+    console.log("No Arguments");
+}else {
+    console.log("Arguments Found");
+}
+}
+let my;
+
