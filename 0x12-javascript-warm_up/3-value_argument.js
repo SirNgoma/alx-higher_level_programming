63f770debfe6546a7a011a9b22622b1504e7a6c8
Themba <seconds.seconds1@gmail.com>
2023-07-22 16:47:25 +0000

    drafg

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,2 +1,11 @@
 #!/usr/bin/node
-
+// Check if there is at least one argument (excluding the script name itself)
+if (process.argv.length < 3) {
+  console.log("Usage: node script_name.js <argument>");
+} else {
+  // Get the first argument passed to the script
+  const firstArgument = process.argv[2];
+  
+  // Print the first argument
+  console.log("First argument:", firstArgument);
+}
--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+// Check if there are exactly two arguments
+if (process.argv.length !== 4) {
+  console.log("Usage: node script_name.js <argument1> <argument2>");
+} else {
+  // Get the two arguments passed to the script
+  const arg1 = process.argv[2];
+  const arg2 = process.argv[3];
+  
+  // Print the arguments in the specified format
+  console.log(`${arg1} is ${arg2}`);
+}
+
